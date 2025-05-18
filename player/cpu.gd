5c58e51ee38e1f4ee26242823d46cc5ef2a74ef5
Angel Roberto Andrade <alroc1234@hotmail.com>
2025-05-18 15:06:13 -0700

    deleted area2d cpu zone and tweaked the speed of the paddles

--- player/cpu.gd
+++ player/cpu.gd
@@ -4,18 +4,10 @@ extends StaticBody2D
 var is_on_cpu_zone = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	if get_tree().get_nodes_in_group("ball") and is_on_cpu_zone:
+func _physics_process(delta: float) -> void:
+	if get_tree().get_nodes_in_group("ball"):
 		if position.y > get_tree().get_nodes_in_group("ball")[0].position.y:
-			position.y -= speed
+			position.y -= speed * delta
 		elif position.y < get_tree().get_nodes_in_group("ball")[0].position.y:
-			position.y += speed
+			position.y += speed * delta
 	position.y = clamp(position.y, 80, 570)
-
-
-func _on_cpu_zone_body_entered(body: Node2D) -> void:
-	is_on_cpu_zone = true
-
-
-func _on_cpu_zone_body_exited(body: Node2D) -> void:
-	is_on_cpu_zone = false
--- player/player2.gd
+++ player/player2.gd
@@ -2,9 +2,9 @@ extends StaticBody2D
 
 @export var speed = 0
 
-func _process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("down_arrow"):
-		position.y += speed 
+		position.y += speed * delta
 	elif Input.is_action_pressed("up_arrow"):
-		position.y -= speed
+		position.y -= speed * delta
 	position.y = clamp(position.y, 80, 570)
